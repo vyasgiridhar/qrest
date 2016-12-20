2097036b198c1ab1e6462413f138b70934509a22
vyasgiridhar <vyasgiridhar27@gmail.com>
2016-12-20 16:26:58 +0530

    Moved functions around

--- adapters/mariadb.go
+++ adapters/mariadb.go
@@ -6,28 +6,56 @@ import (
 
 	"database/sql"
 
-	"github.com/caarlos0/env"
 	//Only init function required
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/vyasgiridhar/qrest/config"
 )
 
+//Query : Returns json data for a Query
+func Query(query string) (jsonData []byte, err error) {
+	return nil, nil
+}
+
 //Conn : Creates a new Database connection
 //and assigns it to db
-func Conn() (db *sql.DB) {
-	cfg := config.Config{}
-	env.Parse(&cfg)
+func Conn(database string) (db *sql.DB) {
 	var err error
-	host := cfg.MDBHost + ":" + strconv.Itoa(cfg.MDBPort)
-	db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", cfg.MDBUser, cfg.MDBPass, host, cfg.MDBDatabase))
+
+	db, err = sql.Open("mysql", PrepareConn(database))
 
 	if err != nil {
-		panic(fmt.Sprintf("Unable to connection to database: %v\n", err))
+		panic(fmt.Sprintf("Unable to connect to database: %v\n", err))
 	}
 	return
 }
 
-//Query : Returns json data for a Query
-func Query(query string) (jsonData []byte, err error) {
-	return nil, nil
+func PrepareQuery(table, field, value string, page, pagesize int) (preparedQuery string) {
+	preparedQuery = ""
+	if table != "" {
+		preparedQuery = fmt.Sprintf("%s %s", SelectFrom, table)
+	}
+	if field != "" {
+		preparedQuery = fmt.Sprintf("%s where %s = ?", preparedQuery, field)
+	}
+	if page != 0 && pagesize != 0 {
+		preparedQuery = fmt.Sprintf("%s limit %d offset %d", preparedQuery, pagesize, page*pagesize)
+	}
+	return
+}
+
+func Process(table, field, value, page, pagesize string) []byte {
+
+	fmt.Println("Processing", table, field, value, page, pagesize)
+
+	db := Conn(config.Conf.MDBDatabase)
+
+	query := PrepareQuery(table, field, value, strconv.Atoi(page), strconv.Atoi(pagesize))
+	x, err := db.Query(query, value)
+	fmt.Println(err)
+	result, _ := JSONify(x)
+	return []byte(result)
+}
+
+func PrepareConn(database string) string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", config.Conf.MDBUser, config.Conf.MDBPass, config.Conf.MDBHost, config.Conf.MDBPort, database)
 }
--- adapters/process.go
+++ adapters/process.go
@@ -1,24 +1,16 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
-	"github.com/vyasgiridhar/qrest/config"
 )
 
 const SelectFrom = `select * from `
-
-func PrepareConn(database string) string {
-	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", config.Conf.MDBUser, config.Conf.MDBPass, config.Conf.MDBHost, config.Conf.MDBPort, database)
-}
 
 func CheckField(table, field string) bool {
 	check := "select COLUMN_NAME from columns where TABLE_NAME = ?"
@@ -36,76 +28,6 @@ func CheckField(table, field string) bool {
 		}
 	}
 	return false
-}
-
-func Conn(database string) (db *sql.DB) {
-	var err error
-
-	db, err = sql.Open("mysql", PrepareConn(database))
-
-	if err != nil {
-		panic(fmt.Sprintf("Unable to connect to database: %v\n", err))
-	}
-	return
-}
-
-func PrepareQuery(table, field, value string, page, pagesize int) (preparedQuery string) {
-	preparedQuery = ""
-	if table != "" {
-		preparedQuery = fmt.Sprintf("%s %s", SelectFrom, table)
-	}
-	if field != "" {
-		preparedQuery = fmt.Sprintf("%s where %s = ?", preparedQuery, field)
-	}
-	if page != 0 && pagesize != 0 {
-		preparedQuery = fmt.Sprintf("%s limit %d offset %d", preparedQuery, pagesize, page*pagesize)
-	}
-	return
-}
-
-func Process(table, field, value, page, pagesize string) []byte {
-	fmt.Println(table, field, value, page, pagesize)
-	db := Conn(config.Conf.MDBDatabase)
-	query := PrepareQuery(table, field, value, strconv.Atoi(page), strconv.Atoi(pagesize))
-	x, err := db.Query(query, value)
-	fmt.Println(err)
-	result, _ := JSONify(x)
-	return []byte(result)
-}
-
-func JSONify(rows *sql.Rows) (string, error) {
-	columns, err := rows.Columns()
-	if err != nil {
-		return "", err
-	}
-	count := len(columns)
-	tableData := make([]map[string]interface{}, 0)
-	values := make([]interface{}, count)
-	valuePtrs := make([]interface{}, count)
-	for rows.Next() {
-		for i := 0; i < count; i++ {
-			valuePtrs[i] = &values[i]
-		}
-		rows.Scan(valuePtrs...)
-		entry := make(map[string]interface{})
-		for i, col := range columns {
-			var v interface{}
-			val := values[i]
-			b, ok := val.([]byte)
-			if ok {
-				v = string(b)
-			} else {
-				v = val
-			}
-			entry[col] = v
-		}
-		tableData = append(tableData, entry)
-	}
-	jsonData, err := json.Marshal(tableData)
-	if err != nil {
-		return "", err
-	}
-	return string(jsonData), nil
 }
 
 func Parse(rw http.ResponseWriter, req *http.Request) {
